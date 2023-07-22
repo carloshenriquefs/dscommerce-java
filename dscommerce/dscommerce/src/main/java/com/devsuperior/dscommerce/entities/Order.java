89280bfc7d64842da10adb65652ad8b40d97cafa
Carlos Henrique <ichfs9108@gmail.com>
2023-07-22 03:05:14 -0300

    OrderItem, OrderItemPK

--- dscommerce/dscommerce/src/main/java/com/devsuperior/dscommerce/entities/Order.java
+++ dscommerce/dscommerce/src/main/java/com/devsuperior/dscommerce/entities/Order.java
@@ -3,6 +3,9 @@ package com.devsuperior.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
@@ -22,6 +25,9 @@ public class Order {
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order() {
     }
@@ -63,5 +69,13 @@ public class Order {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Product> getProducts() {
+        return items.stream().map(x -> x.getProduct()).toList();
     }
 }
