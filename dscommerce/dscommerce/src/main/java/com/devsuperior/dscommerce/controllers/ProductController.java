a9fd56952d2ef3ad64da9ada5dfe7b4dfbe811ea
Carlos Henrique <ichfs9108@gmail.com>
2023-07-29 01:00:52 -0300

    Primeiro teste com Repository

--- dscommerce/dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ dscommerce/dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,15 +1,25 @@
 package com.devsuperior.dscommerce.controllers;
 
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @GetMapping
     public String test() {
-        return "Ola mundo";
+        Optional<Product> result = productRepository.findById(1L);
+        Product product = result.get();
+        return product.getName();
     }
 }
