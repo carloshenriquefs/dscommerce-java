5d1afd553d18ccd5087e4717bb3b1179ee0f47d7
Carlos Henrique <ichfs9108@gmail.com>
2023-07-30 17:53:37 -0300

    Exception product

--- dscommerce/dscommerce/src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ dscommerce/dscommerce/src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -4,6 +4,7 @@ package com.devsuperior.dscommerce.services;
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
+import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,7 +19,8 @@ public class ProductService {
 
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
-        Product product = productRepository.findById(id).get();
+        Product product = productRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Recurso não encontrado"));
         return new ProductDTO(product);
     }
 
