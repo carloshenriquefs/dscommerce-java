fcc034c26d17c2695852763b9acce8e9fe8ef42a
Carlos Henrique <ichfs9108@gmail.com>
2023-07-30 16:25:55 -0300

    Inserindo novo produto com POST

--- dscommerce/dscommerce/src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ dscommerce/dscommerce/src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -28,4 +28,18 @@ public class ProductService {
         return result.map(x -> new ProductDTO(x));
     }
 
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+
+        Product entity = new Product();
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+
+        entity = productRepository.save(entity);
+
+        return new ProductDTO(entity);
+    }
+
 }
