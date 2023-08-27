0cd5c1ac07e0cc557f5acc117830936933c247fc
Carlos Henrique <ichfs9108@gmail.com>
2023-08-27 01:24:17 -0300

    Get user logged

--- dscommerce/dscommerce/src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
+++ dscommerce/dscommerce/src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
@@ -7,11 +7,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findByEmail(String email);
 
     @Query(nativeQuery = true, value = """
 			SELECT tb_user.email AS username, tb_user.password, tb_role.id AS roleId, tb_role.authority
@@ -21,4 +20,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 			WHERE tb_user.email = :email
 		""")
     List<UserDetailsProjection> searchUserAndRolesByEmail(String email);
+
+	Optional<User> findByEmail(String email);
 }
