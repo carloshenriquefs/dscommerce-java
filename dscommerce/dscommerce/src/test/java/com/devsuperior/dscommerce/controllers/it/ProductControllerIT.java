66a61d3f3cc5d5201858c09cbbcbcb8c509015e6
Carlos Henrique <ichfs9108@gmail.com>
2024-02-25 00:55:03 -0300

    Teste inserindo produto com token cliente e token invalido

--- dscommerce/dscommerce/src/test/java/com/devsuperior/dscommerce/controllers/it/ProductControllerIT.java
+++ dscommerce/dscommerce/src/test/java/com/devsuperior/dscommerce/controllers/it/ProductControllerIT.java
@@ -203,4 +203,34 @@ public class ProductControllerIT {
 
         result.andExpect(status().isUnprocessableEntity());
     }
+
+    @Test
+    public void insertShouldReturnForbiddenWhenClientLogged() throws Exception {
+
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+
+        ResultActions result = mockMvc
+                .perform(post("/products")
+                        .header("Authorization", "Bearer " + clientToken)
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void insertShouldReturnUnauthorizedWhenClientLoggedInvalidToken() throws Exception {
+
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+
+        ResultActions result = mockMvc
+                .perform(post("/products")
+                        .header("Authorization", "Bearer " + invalidToken)
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isUnauthorized());
+    }
 }
